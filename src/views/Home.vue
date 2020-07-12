d964c5897a00d011de366897202363afc7873696
Boardonly <dr.yorik@gmail.com>
2020-07-12 22:12:42 +0300

    add catalog-component and search form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="vlp-home">
+    <Search />
+    <v-cataloge />
   </div>
+  <!-- /.vlp-home -->
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header.vue'
+import Header from "@/components/Header.vue";
+import Search from "@/components/home/Search.vue";
+import Cataloge from "@/components/home/Cataloge.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Header
+    Header,
+    Search,
+    "v-cataloge": Cataloge
   }
-}
+};
 </script>
+<style lang="scss">
+</style>
