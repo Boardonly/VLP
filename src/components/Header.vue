419412a21135bea4f98dfc54051bda652c785d4b
Boardonly <dr.yorik@gmail.com>
2020-07-12 13:41:03 +0300

    addHeader.vue and install Foundation

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <nav class="vlp-nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/catalog">Catalog</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
