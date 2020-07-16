033644c297cfe2e241212c0feea2ff0c623d7ca1
Kostya-bilyznyi <konstantin.biliznyy@gmail.com>
2020-07-16 19:29:42 +0300

    first hot fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,27 @@
   <div class="vlp-home">
     <Search />
     <v-cataloge />
+	 <CatalogeMob />
   </div>
   <!-- /.vlp-home -->
 </template>
+
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
 import Search from "@/components/home/Search.vue";
 import Cataloge from "@/components/home/Cataloge.vue";
+import CatalogeMob from "@/components/home/CatalogeMob.vue";
 
 export default {
   name: "Home",
   components: {
-    Header,
-    Search,
-    "v-cataloge": Cataloge
+   Header,
+	Search,
+	"v-cataloge": Cataloge,
+	CatalogeMob
+	 
   }
 };
 </script>
