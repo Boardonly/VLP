d777b144864408297143b66b5427984e6a222bfc
starzhitskiy <study.html.css.java@gmail.com>
2020-07-18 09:57:28 +0300

    test

---
+++ src/components/home/Events.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="vpl-events">
+    <h1>This is Contacts page</h1>
+	 <p>hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Events",
+   data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.vpl-events{
+	background-color: red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="vlp-home">
     <Search />
     <v-cataloge />
+	 <Events />
+    <v-footer />
   </div>
   <!-- /.vlp-home -->
 </template>
@@ -11,13 +13,18 @@
 import Header from "@/components/Header.vue";
 import Search from "@/components/home/Search.vue";
 import Cataloge from "@/components/home/Cataloge.vue";
+import Events from "@/components/home/Events.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-    Search,
+	 Search,
+	 Events,
+	 Footer,
     "v-cataloge": Cataloge
+    
   }
 };
 </script>
