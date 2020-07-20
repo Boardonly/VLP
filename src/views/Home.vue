5b00935d604bd7ce2eb26c9be6061587ff779b89
starzhitskiy <study.html.css.java@gmail.com>
2020-07-21 02:03:44 +0300

    two blok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="vlp-home">
     <Search />
     <v-cataloge />
+	 <Interest />
 	 <Events />
     <v-footer />
   </div>
@@ -13,6 +14,7 @@
 import Header from "@/components/Header.vue";
 import Search from "@/components/home/Search.vue";
 import Cataloge from "@/components/home/Cataloge.vue";
+import Interest from "@/components/home/Interest.vue";
 import Events from "@/components/home/Events.vue";
 import Footer from "@/components/Footer.vue";
 
@@ -21,6 +23,7 @@ export default {
   components: {
     Header,
 	 Search,
+	 Interest,
 	 Events,
 	 Footer,
     "v-cataloge": Cataloge
